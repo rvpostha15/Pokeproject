aa63c466584b7b36d7a31b61c8e625ce513bb0bd
Chase Huber <chasehuber23@gmail.com>
2023-01-04 11:13:33 -0500

    Split code and css into different files in /src/ folder for ease of reading

--- index.js
+++ index.js
@@ -4,15 +4,6 @@
 // Get a reference to the button
 let blueRedToggle = document.querySelector("#blue-red-button");
 
-
-// Get references to the results elements
-let pokeName = document.querySelector("#pokemon-name");
-let pokeId = document.querySelector("#pokemon-id");
-let pokeType = document.querySelector("#pokemon-type");
-let pokeSprite = document.querySelector("#pokemon-sprite");
-let pokeHeight = document.querySelector("#pokemon-height");
-let pokeWeight = document.querySelector("#pokemon-weight");
-
 // Set the initial background color of the button
 blueRedToggle.style.backgroundColor = '#cf1920'; //red
 
@@ -35,23 +26,3 @@ blueRedToggle.addEventListener("click", function() {
     blueRedToggle.textContent = "Pokémon Blue"; //text
   }
 });
-
-//initial fetch: default display pikachu
-fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
-  .then(response => response.json())
-  .then(pokemon => {
- 
-    // Set the text content of the results elements to display the Pokémon data
-    pokeName.textContent = pokemon.name.toUpperCase();
-    pokeId.textContent = "ID: " + pokemon.id;
-    pokeType.textContent = pokemon.types[0].type.name;
-    pokeSprite.src = pokemon.sprites.front_default;
-    pokeHeight.textContent = "Height: " + pokemon.height;
-    pokeWeight.textContent = "Weight: " + pokemon.weight;
-  });
-
-  // document.addEventListener('DOMContentLoaded', () => {
-  //   initialFetch();
-  //   toggle();
-  // })
-
